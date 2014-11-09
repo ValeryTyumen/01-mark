0002ce9332dfac3e45375533da4b59f3507cd55c
Валерий <v.lihosherstov@gmail.com>
2014-11-10 01:01:58 +0500

    Runs all tests, but needs a lot of refactoring

--- Mark/Mark/MarkDownProcessor_should.cs
+++ Mark/Mark/MarkDownProcessor_should.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NUnit;
 using NUnit.Framework;
@@ -13,73 +14,92 @@ namespace Mark
         private IMarkDownProcessor processor;
 
         [SetUp]
-        private void arrange_processor()
+        public void arrange_processor()
         {
+            processor = new MarkDownProcessor();
         }
 
         [Test]
-        public void translate_empty_to_empty()
+        public void tag_one_paragraph()
         {
-            var htmlText = processor.TranslateToHtml("");
-            Assert.AreEqual(htmlText, "");
+            var htmlText = processor.TranslateToHtml("abcd1234");
+            Assert.AreEqual("<p>abcd1234</p>", htmlText);
         }
 
+        
         [Test]
-        public void tag_paragraphs()
+        public void tag_unix_paragraphs()
         {
-            var newLine = Environment.NewLine;
-            var htmlText = processor.TranslateToHtml(String.Format("1{0}{0}2", newLine));
-            Assert.AreEqual(htmlText, "<p>1</p><p>2</p>");
+            var htmlText = processor.TranslateToHtml("1\n\n2");
+            Assert.AreEqual("<p>1</p><p>2</p>", htmlText);
+        }
+        
+        [Test]
+        public void tag_dos_paragraphs()
+        {
+            var htmlText = processor.TranslateToHtml("1\r\n\r\n2");
+            Assert.AreEqual("<p>1</p><p>2</p>", htmlText);
+        }
+        
+        [Test]
+        public void tag_unix_and_dos_paragraphs()
+        {
+            const string text = "1\n\n2\r\n\r\n3\r\n\n4\n\r\n5";
+            const string expectedHtmlText = "<p>1</p><p>2</p><p>3</p><p>4</p><p>5</p>";
+            var htmlText = processor.TranslateToHtml(text);
+            Assert.AreEqual(expectedHtmlText, htmlText);
         }
 
+        
         [Test]
         public void tag_paragraphs_ignoring_whitespaces()
         {
-            var newLine = Environment.NewLine;
-            var htmlText = processor.TranslateToHtml(String.Format("1{0}   {0}2", newLine));
-            Assert.AreEqual(htmlText, "<p>1</p><p>2</p>");
+            const string text = "1\n \n2\r\n  \r\n3\r\n   \n4\n    \r\n5";
+            const string expectedHtmlText = "<p>1</p><p>2</p><p>3</p><p>4</p><p>5</p>";
+            var htmlText = processor.TranslateToHtml(text);
+            Assert.AreEqual(expectedHtmlText, htmlText);
         }
-
+        
         [Test]
         public void tag_cursive_text()
         {
             var htmlText = processor.TranslateToHtml("blah blah _cursive_ blah blah");
-            Assert.AreEqual(htmlText, "blah blah <em>cursive</em> blah blah");
+            Assert.AreEqual("<p>blah blah <em>cursive</em> blah blah</p>", htmlText);
         }
 
         [Test]
-        public void screen_marking_symbols()
+        public void tag_cursive_all_text()
         {
-            var htmlText = processor.TranslateToHtml("\\_normal\\_ \\__cursive_\\_ \\`__bold__\\`");
-            Assert.AreEqual(htmlText, "normal <em>cursive</em> <strong>bold</strong>");
+            var htmlText = processor.TranslateToHtml("_cursive_");
+            Assert.AreEqual("<p><em>cursive</em></p>", htmlText);
         }
 
         [Test]
         public void tag_bold_text()
         {
             var htmlText = processor.TranslateToHtml("blah blah __bold__ blah blah");
-            Assert.AreEqual(htmlText, "blah blah <strong>bold</strong> blah blah");
+            Assert.AreEqual("<p>blah blah <strong>bold</strong> blah blah</p>", htmlText);
+        }
+
+        [Test]
+        public void tag_bold_all_text()
+        {
+            var htmlText = processor.TranslateToHtml("__bold__");
+            Assert.AreEqual("<p><strong>bold</strong></p>", htmlText);
         }
 
         [Test]
         public void tag_cursive_in_bold_text()
-        {
+        { 
             var htmlText = processor.TranslateToHtml("blah __blah _blah_ blah__ blah");
-            Assert.AreEqual(htmlText, "blah <strong>blah <em>blah</em> blah</strong> blah");
+            Assert.AreEqual("<p>blah <strong>blah <em>blah</em> blah</strong> blah</p>", htmlText);
         }
 
         [Test]
         public void tag_bold_in_cursive_text()
         {
             var htmlText = processor.TranslateToHtml("blah _blah __blah__ blah_ blah");
-            Assert.AreEqual(htmlText, "blah <em>blah <strong>blah</strong> blah</em> blah");
-        }
-
-        [Test]
-        public void tag_coded_text()
-        {
-            var htmlText = processor.TranslateToHtml("blah `blah _blah_ blah` blah");
-            Assert.AreEqual(htmlText, "blah <code>blah blah blah</code> blah");
+            Assert.AreEqual("<p>blah <em>blah <strong>blah</strong> blah</em> blah</p>", htmlText);
         }
 
         [Test]
@@ -87,15 +107,36 @@ namespace Mark
         {
             const string text = "blah_blah__blah___blah 1_2__3___4";
             var htmlText = processor.TranslateToHtml(text);
-            Assert.AreEqual(htmlText, text);
+            Assert.AreEqual(htmlText, "<p>" + text + "</p>");
+        }
+        
+        [Test]
+        public void screen_cursive_marks()
+        {
+            var htmlText = processor.TranslateToHtml("\\_1\\_ _3 4\\_ 5_");
+            Assert.AreEqual("<p>_1_ <em>3 4_ 5</em></p>", htmlText);
         }
 
+        [Test]
+        public void screen_bold_marks()
+        {
+            var htmlText = processor.TranslateToHtml("\\__1\\__ __3 4\\__ 5__");
+            Assert.AreEqual("<p>__1__ <strong>3 4__ 5</strong></p>", htmlText);
+        }
+
+        [Test]
+        public void tag_coded_text()
+        {
+            var htmlText = processor.TranslateToHtml("blah `blah _blah_ __blah__ blah` blah");
+            Assert.AreEqual("<p>blah <code>blah _blah_ __blah__ blah</code> blah</p>", htmlText);
+        }
+        
         [Test]
         public void not_tag_unpaired_marking_symbols()
         {
             const string text = "blah _blah `blah __blah blah";
             var htmlText = processor.TranslateToHtml(text);
-            Assert.AreEqual(htmlText, text);
+            Assert.AreEqual("<p>" + text + "</p>", htmlText);
         }
     }
 }
--- Mark/Mark/Program.cs
+++ Mark/Mark/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -18,5 +20,194 @@ namespace Mark
     public interface IMarkDownProcessor
     {
         string TranslateToHtml(string text);
+    }
+
+
+
+    public class MarkDownProcessor : IMarkDownProcessor
+    {
+        private static string TranslateNewlines(string text)
+        {
+            var newParagraphRegex = new Regex(@"\r?\n\s*\r?\n");
+            newParagraphRegex.Replace(text, "</p><p>");
+            return "<p>" + newParagraphRegex.Replace(text, "</p><p>") + "</p>";
+        }
+
+        private enum Tag
+        {
+            Em,
+            Strong,
+            Code
+        };
+
+        private readonly static Dictionary<Tag, Regex> MarkingUnderscoreRegexes =
+            new Dictionary<Tag, Regex>
+        {
+            {Tag.Em, new Regex(@"((?<![\w_])_(?![\W_])|(?<=[^\W_]|\\)_(?![\w_]))")},
+            {Tag.Strong, new Regex(@"((?<=\W)__(?=\w)|(?<=\w|\\)__(?=\W))")}
+        };
+
+        private readonly static Dictionary<Tag, string> TagNames =
+            new Dictionary<Tag, string>
+        {
+            {Tag.Em, "em"},
+            {Tag.Strong, "strong"},
+            {Tag.Code, "code"}
+        };
+
+        private static string TranslateUnderscores(string text, Tag marking)
+        {
+            var markingUnderscoreRegex = MarkingUnderscoreRegexes[marking];
+            var builder = new StringBuilder();
+            var match = markingUnderscoreRegex.Match(text);
+            if (! match.Success)
+                return text;
+            var lastMatchIndex = 0;
+            while (match.Success)
+            {
+                builder.Append(text.Substring(lastMatchIndex, match.Index - lastMatchIndex));
+                lastMatchIndex = match.Index + match.Length;
+                builder.Append(TranslateOneUnderscoreMark(text, match.Index, marking));
+                match = match.NextMatch();
+            }
+            builder.Append(text.Substring(lastMatchIndex, text.Length - lastMatchIndex));
+            return builder.ToString();
+        }
+
+
+        private readonly static Regex LetterRegex = new Regex(@"([^\W_]|\\)");
+
+        private static bool IsLetter(char sign)
+        {
+            string signString = sign.ToString();
+            return LetterRegex.Match(signString).ToString() == signString;
+        }
+
+        private static string TranslateOneUnderscoreMark
+            (string text, int index, Tag marking)
+        {
+            var markLength = (marking == Tag.Em) ? 1 : 2;
+            var tag = TagNames[marking];
+            if (IsLetter(text[index - 1]))
+                return "</" + tag + ">";
+            else
+                return "<" + tag + ">";
+        }
+
+        private static string ScreenSomeUnderscores(string text, Tag marking)
+        {
+            var wrongTagRegex = new Regex(@"\\</?" + TagNames[marking] + @">");
+            if (marking == Tag.Em)
+                return wrongTagRegex.Replace(text, "_");
+            else
+                return wrongTagRegex.Replace(text, "__");
+        }
+
+        public static string TranslateBackticks(string text)
+        {
+            var builder = new StringBuilder();
+            var lastBacktickIndex = 0;
+            var lastWasClosing = true;
+            for (var i = 0; i < text.Length; i++)
+                if (text[i] == '`')
+                {
+                    if (lastWasClosing)
+                    {
+                        builder.Append(text.Substring(lastBacktickIndex, i - lastBacktickIndex));
+                        builder.Append("<code>");
+                        lastBacktickIndex = i + 1;
+                        lastWasClosing = false;
+                    }
+                    else
+                    {
+                        var subString = text.Substring(lastBacktickIndex, i - lastBacktickIndex);
+                        builder.Append(TranslateOnePairOfBackticks(subString));
+                        builder.Append("</code>");
+                        lastBacktickIndex = i + 1;
+                        lastWasClosing = true;
+                    }
+                }
+            var tail = text.Substring(lastBacktickIndex, text.Length - lastBacktickIndex);
+            builder.Append(tail);
+            return builder.ToString();
+        }
+
+        public static string TranslateOnePairOfBackticks(string text)
+        {
+            text = text.Replace("<em>", "_");
+            text = text.Replace("</em>", "_");
+            text = text.Replace("<strong>", "__");
+            text = text.Replace("</strong>", "__");
+            return text;
+        }
+
+        private static string RemoveUnpairedTags(string text, Tag marking)
+        {
+            var builder = new StringBuilder();
+            var mark = "_";
+            if (marking == Tag.Strong)
+                mark = "__";
+            if (marking == Tag.Code)
+                mark = "`";
+            var openingTag = "<" + TagNames[marking] + ">";
+            var tagRegex = new Regex(@"</?" + TagNames[marking] + ">");
+            var lastWasClosing = true;
+            var lastCorrectMatchIndex = 0;
+            foreach (Match match in tagRegex.Matches(text))
+            {
+                if (lastWasClosing)
+                {
+                    if (match.Value == openingTag)
+                    {
+                        var substring = text.Substring(lastCorrectMatchIndex, match.Index - lastCorrectMatchIndex);
+                        builder.Append(RemoveAllTags(substring, marking));
+                        lastWasClosing = false;
+                        lastCorrectMatchIndex = match.Index + match.Length;
+                    }
+                }
+                else
+                {
+                    if (match.Value != openingTag)
+                    {
+                        var substring = text.Substring(lastCorrectMatchIndex, match.Index - lastCorrectMatchIndex);
+                        builder.Append(openingTag);
+                        builder.Append(RemoveAllTags(substring, marking));
+                        builder.Append(match.Value);
+                        lastWasClosing = true;
+                        lastCorrectMatchIndex = match.Index + match.Length;
+                    }
+                }
+            }
+            if (!lastWasClosing)
+                builder.Append(mark);
+            var tail = text.Substring(lastCorrectMatchIndex, text.Length - lastCorrectMatchIndex);
+            builder.Append(RemoveAllTags(tail, marking));
+            return builder.ToString();
+        }
+
+        private static string RemoveAllTags(string text, Tag marking)
+        {
+            var mark = "_";
+            if (marking == Tag.Strong)
+                mark = "__";
+            if (marking == Tag.Code)
+                mark = "`";
+            var tagRegex = new Regex(@"</?" + TagNames[marking] + ">");
+            return tagRegex.Replace(text, mark);
+        }
+
+        public string TranslateToHtml(string text)
+        {
+            text = TranslateNewlines(text);
+            text = TranslateUnderscores(text, Tag.Em);
+            text = TranslateUnderscores(text, Tag.Strong);
+            text = ScreenSomeUnderscores(text, Tag.Em);
+            text = ScreenSomeUnderscores(text, Tag.Strong);
+            text = RemoveUnpairedTags(text, Tag.Em);
+            text = RemoveUnpairedTags(text, Tag.Strong);
+            text = TranslateBackticks(text);
+            text = RemoveUnpairedTags(text, Tag.Code);
+            return text;
+        }
     }
 }
